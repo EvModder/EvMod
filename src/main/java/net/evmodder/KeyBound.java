73ab022a9eb8cb8f5e7461b727f3d737562e6a1e
EvModder <evmodder@gmail.com>
2024-08-22 21:56:16 -0500

    Add color-scroll feature & update deps

--- src/main/java/net/evmodder/KeyBound.java
+++ src/main/java/net/evmodder/KeyBound.java
@@ -3,7 +3,10 @@ package net.evmodder;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ClientModInitializer;
@@ -12,7 +15,11 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.InputUtil.Type;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.PlayerModelPart;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registries;
 
 // Export jar from Terminal:
 // gradle build --refresh-dependencies
@@ -23,8 +30,9 @@ public class KeyBound implements ClientModInitializer{
 	// Reference variables
 	public static final String MOD_ID = "keybound";
 	public static final String MOD_NAME = "KeyBound";
-	public static final String MOD_VERSION = "@MOD_VERSION@";
+	//public static final String MOD_VERSION = "@MOD_VERSION@";
 	private final String SKIN_LAYER_CATEGORY = "category."+MOD_ID+".skin_toggles";
+	private final String COLOR_SCROLL_CATEGORY = "category."+MOD_ID+".color_scroll";
 	private final String CHAT_MSG_CATEGORY = "category."+MOD_ID+".chat_messages";
 	private final String BOT_MSG_CATEGORY = "category."+MOD_ID+".bot_messages";
 
@@ -33,6 +41,9 @@ public class KeyBound implements ClientModInitializer{
 
 	private int CLIENT_ID;
 	private String CLIENT_KEY;
+
+	private String[] colors;
+	private Set<String> coloredItems; // replace * with color_name
 
 	private static class EvKeyBinding extends KeyBinding{
 		public EvKeyBinding(String translationKey, Type type, int code, String category){super(translationKey, type, code, category);}
@@ -51,6 +62,38 @@ public class KeyBound implements ClientModInitializer{
 			@Override public void onPressed(){
 				final MinecraftClient client = MinecraftClient.getInstance();
 				client.options.togglePlayerModelPart(part, !client.options.isPlayerModelPartEnabled(part));
+				//client.player.sendMessage(net.minecraft.text.Text.literal("Key was pressed! part:"+part.name().toLowerCase()));
+			}
+		}).forEach(KeyBindingHelper::registerKeyBinding);
+	}
+	private void registerColorScrollKeybinds(){
+		Stream.of(true, false).map(k -> new EvKeyBinding("key."+MOD_ID+".color_scroll_"+(k ? "up" : "down"), InputUtil.Type.KEYSYM, -1, COLOR_SCROLL_CATEGORY){
+			@Override public void onPressed(){
+				LOGGER.info("color scroll key pressed for: "+k);
+				final MinecraftClient client = MinecraftClient.getInstance();
+				PlayerInventory inventory = client.player.getInventory();
+				if(!PlayerInventory.isValidHotbarIndex(inventory.selectedSlot)) return;
+				LOGGER.info("valid hotbar index");
+				ItemStack item = inventory.getMainHandStack();
+				Identifier id = Registries.ITEM.getId(item.getItem());
+				if(!ItemStack.areItemsAndComponentsEqual(item, new ItemStack(Registries.ITEM.get(id)))) return;  // don't scroll if has custom NBT
+				LOGGER.info("no custom components");
+				String path = id.getPath();
+				int i = 0;
+				while(!path.contains(colors[i]+"_") && ++i < colors.length);
+				if(i == colors.length) return;  // color not found
+				LOGGER.info("color found");
+				if(!coloredItems.contains(id.getNamespace()+":"+path.replaceFirst(colors[i], "*"))) return; // not a supported item (eg: "red_sandstone")
+				LOGGER.info("item is supported");
+
+				int new_i = k ? (i == colors.length-1 ? 0 : i+1) : (i == 0 ? colors.length-1 : i-1);
+				id = Identifier.of(id.getNamespace(), id.getPath().replaceFirst(colors[i], colors[new_i]));
+				if(client.player.isInCreativeMode()) inventory.setStack(inventory.selectedSlot, new ItemStack(Registries.ITEM.get(id)));
+				else{
+					LOGGER.info("not creative...");
+					inventory.getSwappableHotbarSlot();
+					//TODO: find next color in the cycle available from inventory
+				}
 			}
 		}).forEach(KeyBindingHelper::registerKeyBinding);
 	}
@@ -101,17 +144,41 @@ public class KeyBound implements ClientModInitializer{
 		//LOGGER.info("added bot msg keybind "+config.get(key).toUpperCase()+" for: "+message);
 	}
 
+	private void loadColoredItems(){
+		final String[] subArr = Arrays.copyOfRange(colors, 1, colors.length);
+		coloredItems = Registries.ITEM.getIds().stream()
+			.filter(id -> id.getPath().contains(colors[0]))
+			.filter(id -> Arrays.stream(subArr).allMatch(
+				c -> Registries.ITEM.containsId(Identifier.of(id.getNamespace(), id.getPath().replaceFirst(colors[0], c)))
+			)).map(id -> id.getNamespace()+":"+id.getPath().replaceFirst(colors[0], "*"))
+			.collect(Collectors.toSet());
+	}
+
 	private void loadConfig(){
 		config = new HashMap<>();
 		final String configContents = FileIO.loadFile("keybound.txt", getClass().getResourceAsStream("/keybound.txt"));
+		String listKey = null, listValue = null;
 		for(String line : configContents.split("\\r?\\n")){
+			if(listKey != null){
+				line = line.trim();
+				listValue += line;
+				if(line.charAt(line.length()-1) == ']'){
+					config.put(listKey, listValue);
+					listKey = null;
+				}
+				continue;
+			}
 			final int sep = line.indexOf(':');
 			if(sep == -1) continue;
 			final String key = line.substring(0, sep).trim();
 			final String value = line.substring(sep+1).trim();
 			if(key.isEmpty() || value.isEmpty()) continue;
+			if(value.charAt(0) == '[' && value.charAt(value.length()-1) != ']'){
+				listKey = key; listValue = value;
+			}
 			config.put(key, value);
 		}
+		if(listKey != null) LOGGER.error("Unterminated list in ./config/keybound.txt!\nkey: "+listKey);
 
 		final HashMap<String, String> chatMsgLookup = new HashMap<>();
 		final HashMap<String, UUID> botMsgLookup = new HashMap<>();
@@ -136,6 +203,13 @@ public class KeyBound implements ClientModInitializer{
 					break;
 				case "client_key":
 					CLIENT_KEY = config.get(key);
+					break;
+				case "color_scroll_order":
+					colors = config.get(key).split("\\s*,\\s*");
+					LOGGER.info("loaded supported colors: "+String.join(", ", colors));
+					loadColoredItems();
+					LOGGER.info("loaded supported items: "+String.join(", ", coloredItems));
+					registerColorScrollKeybinds();
 					break;
 				default:
 					LOGGER.warn("Unrecognized config setting: "+key);
