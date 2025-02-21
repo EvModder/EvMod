bdf1c39a1120da86297d6f3341e8a6120638c8a8
EvModder <evmodder@gmail.com>
2025-02-21 02:46:13 -0600

    Remove commented-out code: old getNextByName() and pre/post process right click

--- src/main/java/net/evmodder/KeyBound/MapHandRestock.java
+++ src/main/java/net/evmodder/KeyBound/MapHandRestock.java
@@ -27,23 +27,6 @@ public final class MapHandRestock{
 		return stack == null || stack.getCustomName() == null ? null : stack.getCustomName().getLiteralString();
 	}
 
-//	private record SubstrIndices(int a, int b){}
-//	private SubstrIndices longestCommonSubstr(String a, String b){
-//		int[] lens = new int[b.length()];
-//
-//		int slen=0, si=0;
-//		for(int i=0; i<a.length(); ++i){
-//			for(int j=0; j<b.length(); ++j) {
-//				if(a.charAt(i) == b.charAt(j)){
-//					if(++lens[j] > slen){slen = lens[j]; si = i+1;}
-//				}
-//				else lens[j] = 0;
-//			}
-//		}
-//		return new SubstrIndices(si-slen, si);
-//	}
-
-	// 0 = no. 1 = maybe (L->R), 3 = probably (L->M), 4 = definitely (4->5)
 	//TODO: passing metadata, particularly NxM if known
 	private int checkComesAfter(String posA, String posB){
 		if(posA.isBlank() || posB.isBlank() || posA.equals(posB)) return 1; // "Map"->"Map p2", "Map start"->"Map"
@@ -91,9 +74,7 @@ public final class MapHandRestock{
 
 	private String simplifyPosStr(String rawPos){
 		String pos = Normalizer.normalize(
-				rawPos.replaceAll("[^\\p{IsAlphabetic}\\p{IsDigit}]+", " ").trim()
-//					.replaceAll("\\p{IsAlphabetic}\\p{IsAlphabetic}", "$1 $2")
-					,
+				rawPos.replaceAll("[^\\p{IsAlphabetic}\\p{IsDigit}]+", " ").trim(),
 				Normalizer.Form.NFD).toUpperCase();
 		while(pos.matches(".*[A-Z][A-Z].*")) pos = pos.replaceAll("([A-Z])([A-Z])", "$1 $2");
 		return pos;
@@ -201,95 +182,16 @@ public final class MapHandRestock{
 		else restockFromSlot = -1;
 		if(restockFromSlot == -1){Main.LOGGER.info("MapRestock: unable to find next map"); return;}
 
-//		SubstrIndices common = new SubstrIndices(0, 0);
-//		for(int i=9; i<=45; ++i){
-//			ItemStack item = player.playerScreenHandler.getSlot(i).getStack();
-//			if(!isSingleMapArt(item)) continue;
-//			final String name = getCustomName(item);
-//			if(name == null || name.equals(prevName)) continue;
-//			//if(item.equals(prevMap)) continue;
-//			SubstrIndices ab = longestCommonSubstr(prevName, name);
-//			if(ab.a != 0 && ab.b != prevName.length()) continue; // We can only handle 2 parts: {shared name | pos data}
-//			final boolean oldContainsNew = common.b >= ab.b && common.a <= ab.a;
-//			final boolean newContainsOld = ab.b >= common.b && ab.a <= common.a;
-//			final int oldLen = common.b-common.a, newLen = ab.b-ab.a; // old="Map_01,", new="Map_", input=[Map_01,01 Map_01,02 Map_02,01 Map_02,02]
-//			if(!newContainsOld && newLen > oldLen) common = ab;
-//			else if(oldContainsNew && newLen < oldLen && (ab.a==0)==(common.a==0)) common = ab;
-//		}
-//		if(common.a == 0 && common.b == 0){Main.LOGGER.info("MapRestock: No shared-prefix/suffix maps found"); return;}
-//
-//		Main.LOGGER.info("MapRestock: prevMap name: "+prevName.substring(common.a, common.b));
-////		final String posA, posB;
-////		if(common.a == 0){posA = prevName.substring(common.b); posB = prevName.substring(common.b);}
-////		else{posA = prevName.substring(0, common.a); posB = prevName.substring(0, prevName.length()-(common.b-common.a));}
-//		final String prevRawPos = common.a == 0 ? prevName.substring(0, common.b) : prevName.substring(0, common.a);
-//		final String prevPos = prevRawPos.replaceAll("[^\\p{IsAlphabetic}\\p{IsDigit}]+", " ").replaceAll("\\p{IsAlphabetic}\\p{IsAlphabetic}", "$1 $2").trim();
-//		Main.LOGGER.info("MapRestock: prevMap pos: "+prevPos);
-//		final int split2d = prevPos.indexOf(' ');
-//		if(split2d != -1 && prevPos.indexOf(' ', split2d+1) != -1){Main.LOGGER.info("MapRestock: unrecognized pos data"); return;}
-//
-//		// Figure out how many maps with matching name are in inventory
-//		final int commonLen = common.b-common.a;
-//		ArrayList<Integer> slotsWithMatchingMaps = new ArrayList<>();
-//		for(int i=9; i<=45; ++i){
-//			ItemStack item = player.playerScreenHandler.getSlot(i).getStack();
-//			if(!isSingleMapArt(item)) continue;
-//			final String name = getCustomName(item);
-//			if(name == null || name.length() < commonLen) continue;
-//			final int startB = common.a == 0 ? 0 : name.length()-commonLen;
-//			if(prevName.regionMatches(common.a, name, startB, commonLen)){
-//				slotsWithMatchingMaps.add(i);
-//				final String pos = simplifyPosStr(common.a == 0 ? name.substring(common.b) : name.substring(0, name.length()-commonLen));
-//				final int split2di = pos.indexOf(' ');
-//				if((split2d==-1) != (split2di==-1)){Main.LOGGER.info("MapRestock: mismatched pos data: "+name); return;}
-//				if(split2di != -1 && pos.indexOf(' ', split2di+1) != -1){Main.LOGGER.info("MapRestock: unrecognized pos data: "+name); return;}
-//			}
-//		}
-//		Main.LOGGER.info("MapRestock: Num maps with common name: "+slotsWithMatchingMaps.size());
-//		assert slotsWithMatchingMaps.size() > 1;
-//
-////		// Calculate width/height (although we won't know which is which... need img processing mode for that)
-////		final int dim1, dim2;
-////		if(split2d == -1) dim1 = dim2 = split2d;
-////		else{
-////			int dim1Min=Integer.MAX_VALUE, dim1Max=Integer.MIN_VALUE, dim2Min=Integer.MAX_VALUE, dim2Max=Integer.MIN_VALUE;
-////			for(int slot : slotsWithMatchingMaps){
-////				final String name = getCustomName(player.playerScreenHandler.getSlot(slot).getStack());
-////				final String pos = simplifyPosStr(common.a == 0 ? name.substring(common.b) : name.substring(0, name.length()-commonLen));
-////				final int cutIdx = pos.indexOf(' ');
-////				if(cutIdx == -1){Main.LOGGER.info("MapRestock: inconsistent pos names (some are 2D, some are not)"); return;}
-////				final String pos1 = pos.substring(0, cutIdx), pos2 = pos.substring(cutIdx+1);
-////				int pos1i = posAsInt(pos1), pos2i = posAsInt(pos2);
-////				//if(pos1i == -1 || pos2i == -1){Main.LOGGER.info("MapRestock: unrecognized pos data"); return;}
-////				assert pos1i != -1 && pos2i != -1;
-////				dim1Min = Math.min(dim1Min, pos1i); dim1Max = Math.max(dim1Max, pos1i);
-////				dim2Min = Math.min(dim2Min, pos2i); dim2Max = Math.max(dim2Max, pos2i);
-////			}
-////			dim1 = dim1Max - dim1Min;
-////			dim2 = dim2Max - dim2Min;
-////		}
-//		//if(split2d != -1){Main.LOGGER.info("MapRestock: TODO currently does not support 2d map args"); return;}
-//
-//		int bestSlot = -1, bestConfidence = -1;
-//		for(int slot : slotsWithMatchingMaps){
-//			final String name = getCustomName(player.playerScreenHandler.getSlot(slot).getStack());
-//			final String pos = simplifyPosStr(common.a == 0 ? name.substring(common.b) : name.substring(0, name.length()-commonLen));
-//			final int confidence = checkComesAfter(prevPos, pos);
-//			if(confidence > bestConfidence){bestConfidence = confidence; bestSlot = slot;}
-//		}
-//		if(bestSlot == -1){Main.LOGGER.info("MapRestock: Could not find nextMap with high enough confidence"); return;}
-//		else{
-			MinecraftClient client = MinecraftClient.getInstance();
-			if(restockFromSlot >= 36 && restockFromSlot < 45){
-				player.getInventory().selectedSlot = restockFromSlot - 36;
-				client.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(player.getInventory().selectedSlot));
-				Main.LOGGER.info("MapRestock: Changed selected hotbar slot to nextMap");
-			}
-			else{
-				client.interactionManager.clickSlot(0, restockFromSlot, player.getInventory().selectedSlot, SlotActionType.SWAP, player);
-				Main.LOGGER.info("MapRestock: Swapped nextMap int inv.selectedSlot");
-			}
-//		}
+		MinecraftClient client = MinecraftClient.getInstance();
+		if(restockFromSlot >= 36 && restockFromSlot < 45){
+			player.getInventory().selectedSlot = restockFromSlot - 36;
+			client.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(player.getInventory().selectedSlot));
+			Main.LOGGER.info("MapRestock: Changed selected hotbar slot to nextMap");
+		}
+		else{
+			client.interactionManager.clickSlot(0, restockFromSlot, player.getInventory().selectedSlot, SlotActionType.SWAP, player);
+			Main.LOGGER.info("MapRestock: Swapped nextMap int inv.selectedSlot");
+		}
 	}
 
 	MapHandRestock(boolean useName, boolean useImg){
@@ -312,22 +214,4 @@ public final class MapHandRestock{
 			return ActionResult.PASS;
 		});
 	}
-
-	/*
-	public static boolean isEnabled = false;
-	private static ItemStack placedMap;
-
-	public static void onProcessRightClickPre(PlayerEntity player, Hand hand){
-		ItemStack stack = player.getStackInHand(hand);
-		if(isSingleMapArt(stack)){
-			Main.LOGGER.info("Right clicked with a single map");
-			placedMap = stack.copy();
-		}
-	}
-
-	public static void onProcessRightClickPost(PlayerEntity player, Hand hand){
-		Main.LOGGER.info("item in hand (post-process): "+player.getStackInHand(hand).getItem().getName()+", "+player.getStackInHand(hand).getCount());
-		if(placedMap != null && player.getStackInHand(hand).isEmpty()) tryToStockNextMap(player, hand, placedMap);
-		placedMap = null;
-	}*/
 }
