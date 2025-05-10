f12052be0e707c9876739c02e6e0e7ee457709e7
EvModder <evmodder@gmail.com>
2025-05-10 14:39:01 -0500

    Draft for auto-swap low-dur elytra

--- src/main/java/net/evmodder/KeyBound/Keybinds/KeybindAIETravelHelper.java
+++ src/main/java/net/evmodder/KeyBound/Keybinds/KeybindAIETravelHelper.java
@@ -3,21 +3,25 @@ package net.evmodder.KeyBound.Keybinds;
 import net.evmodder.EvLib.TextUtils;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.SlotActionType;
 
 public final class KeybindAIETravelHelper{
 	private boolean isEnabled;
 	private final MinecraftClient client;
-	private final int kickBelowY = 80, kickBelowDur = 20;
+	private final int unsafeDur = 20, unsafeY = 80, kickY = 50;
 	private final long dontKickIfEnabledInLastMs = 10_000l;
 	private long enabledTs;
 	private final boolean waitForSafePitch = true;
 	private final float safePitchUpper = -1, safePitchLower = -10, setSafePitch = -5;
 	private float lastPitch;
+
+	private final boolean isFreshElytra(ItemStack stack){
+		return stack.getItem() == Items.ELYTRA && stack.getMaxDamage() - stack.getDamage() > unsafeDur+10;
+	}
 
 	public KeybindAIETravelHelper(){
 		client = MinecraftClient.getInstance();
@@ -26,30 +30,46 @@ public final class KeybindAIETravelHelper{
 			client.player.sendMessage(Text.of(status), true);
 			enabledTs = System.currentTimeMillis();
 		}));
+		assert kickY < unsafeY;
+		assert safePitchLower < setSafePitch && setSafePitch < safePitchUpper;
 
 		ClientTickEvents.START_CLIENT_TICK.register(_ -> {
 			if(!isEnabled) return;
 			if(client.player == null || client.world == null){isEnabled = false; return;}
 
 			ItemStack chestStack = client.player.getInventory().getArmorStack(2);
-			Identifier chestItemId = Registries.ITEM.getId(chestStack.getItem());
-			if(chestItemId == null || !chestItemId.getPath().equals("elytra")){
+			//Identifier chestItemId = Registries.ITEM.getId(chestStack.getItem());
+			if(chestStack.getItem() != Items.ELYTRA){
 				client.player.sendMessage(Text.of("Not wearing elytra"), true);
 				isEnabled = false;
 				return;
 			}
 			final int durability = chestStack.getMaxDamage() - chestStack.getDamage();
-			final boolean tooLowDur = durability <= kickBelowDur;
-			final boolean tooLowY = client.player.getBlockY() <= kickBelowY;
+			final boolean tooLowDur = durability <= unsafeDur;
+			final boolean tooLowY = client.player.getBlockY() <= unsafeY;
 			final float pitch = client.player.getPitch();
 
 			if(tooLowY || tooLowDur){
 				if(tooLowY){
 					client.player.setPitch(setSafePitch);
+					if(client.player.getBlockY() > kickY) return;
 				}
-				else if(waitForSafePitch && (pitch > safePitchLower || pitch > safePitchUpper || pitch <= lastPitch)){
-					client.player.sendMessage(Text.of("Current AIE helper settings will trigger disconnect due to too low dur"), true);
-					return;
+				else{
+					for(int i=9; i<45; ++i) if(isFreshElytra(client.player.getInventory().getStack(i%36))){
+						if(i < 36){
+							client.interactionManager.clickSlot(0, i, 1, SlotActionType.PICKUP, client.player); // Pickup fresh elytra
+							client.interactionManager.clickSlot(0, 6, 0, SlotActionType.PICKUP, client.player); // Put in armor slot
+							client.interactionManager.clickSlot(0, i, 0, SlotActionType.PICKUP, client.player); // Put back used elytra
+						}
+						else{
+							client.interactionManager.clickSlot(0, 6, i-36, SlotActionType.SWAP, client.player); // Swap with hotbar
+						}
+						return;
+					}
+					if(waitForSafePitch && (pitch < safePitchLower || pitch > safePitchUpper || pitch <= lastPitch)){
+						client.player.sendMessage(Text.of("Current AIE helper settings will trigger disconnect due to too low dur"), true);
+						return;
+					}
 				}
 				final long timeSinceEnabled = System.currentTimeMillis() - enabledTs;
 				if(timeSinceEnabled <= dontKickIfEnabledInLastMs){
