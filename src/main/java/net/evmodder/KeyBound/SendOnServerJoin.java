a844d796531f3e51b72d0c9f8252b163a93cc335
EvModder <evmodder@gmail.com>
2025-05-13 12:37:40 -0500

    Wait for player/world/queue to load before sending join_messages

--- src/main/java/net/evmodder/KeyBound/SendOnServerJoin.java
+++ src/main/java/net/evmodder/KeyBound/SendOnServerJoin.java
@@ -1,20 +1,30 @@
 package net.evmodder.KeyBound;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 
 public class SendOnServerJoin{
 	SendOnServerJoin(String[] messages){
+		Main.LOGGER.info("waiting for world to load...");
 		ServerPlayConnectionEvents.JOIN.register(
 				//ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server
 				(_, _, _) ->
 		{
-			ClientPlayNetworkHandler handler = MinecraftClient.getInstance().getNetworkHandler();
-			for(String msg : messages){
-				if(msg.startsWith("/")) handler.sendChatCommand(msg.substring(1));
-				else handler.sendChatCommand(msg);
-			}
+			new Timer().schedule(new TimerTask(){@Override public void run(){
+				MinecraftClient client  = MinecraftClient.getInstance();
+				if(client.player == null || client.world == null || client.getNetworkHandler() == null
+						|| client.player.isSpectator()) return; // Wait for stuff to load properly
+
+				ClientPlayNetworkHandler handler = client.getNetworkHandler();
+				for(String msg : messages){
+					if(msg.startsWith("/")) handler.sendChatCommand(msg.substring(1));
+					else handler.sendChatCommand(msg);
+				}
+				cancel();
+			}}, 1000l, 1000l);
 		});
 	}
 }
