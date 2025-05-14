fe3d6c4325246f6991b3cc8b69d5612f2c601de1
EvModder <evmodder@gmail.com>
2025-05-14 11:24:24 -0500

    More cleanup for SendOnJoin

--- src/main/java/net/evmodder/KeyBound/SendOnServerJoin.java
+++ src/main/java/net/evmodder/KeyBound/SendOnServerJoin.java
@@ -5,41 +5,43 @@ import java.util.TimerTask;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.text.Text;
 
 public class SendOnServerJoin{
 	private final long JOIN_DELAY = 2000l;
-	private boolean isJoining;
-	private long joinedAt;
+	private long loadedAt/*, joinedAt*/;
+	private TimerTask timerTask;
 
 	SendOnServerJoin(String[] messages){
 		ClientPlayConnectionEvents.JOIN.register(
 				//ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server
 				(_0, _1, _2) ->
 		{
-			if(isJoining) return;
-			isJoining = true;
-			new Timer().schedule(new TimerTask(){@Override public void run(){
-				Main.LOGGER.info("Server join detected, JOIN_DELAY is over, checking if stuff is loaded");
-				MinecraftClient client  = MinecraftClient.getInstance();
+			if(timerTask != null) timerTask.cancel(); // Restart timer
 
-				// Wait for stuff to load properly
+			//joinedAt = System.currentTimeMillis();
+			new Timer().schedule(timerTask = new TimerTask(){@Override public void run(){
+				//final long now = System.currentTimeMillis();
+				//if(now - joinedAt > GIVE_UP_AFTER_MS) cancel();
+
+				//Main.LOGGER.info("Server join detected, checking if stuff is loaded");
+				MinecraftClient client  = MinecraftClient.getInstance();
 				if(!client.isFinishedLoading() || client.player == null || client.world == null || client.getNetworkHandler() == null || !client.player.isAlive()
 					|| !client.player.isLoaded() || client.player.isSpectator() || client.player.isRegionUnloaded() || client.player.isInvisible()) return;
-				final long now = System.currentTimeMillis();
-				if(joinedAt == 0){joinedAt = now; return;}
-				else if(now - joinedAt < JOIN_DELAY) return;
 
-				Main.LOGGER.info("Server join detected, stuff seems loaded, triggering commands...");
+				//Main.LOGGER.info("Stuff seems loaded, checking JOIN_DELAY");
+				final long now = System.currentTimeMillis();
+				if(loadedAt == 0){loadedAt = now; return;}
+				else if(now - loadedAt < JOIN_DELAY) return;
+
+				//Main.LOGGER.info("JOIN_DELAY reached, triggering commands...");
 				ClientPlayNetworkHandler handler = client.getNetworkHandler();
 				for(String msg : messages){
 					msg = msg.trim();
-					client.player.sendMessage(Text.of("Sending "+(msg.startsWith("/")?"cmd":"msg")+": "+msg), false);
+					//client.player.sendMessage(Text.of("Sending "+(msg.startsWith("/")?"cmd":"msg")+": "+msg), false);
 					if(msg.startsWith("/")) handler.sendChatCommand(msg.substring(1));
 					else handler.sendChatMessage(msg);
 				}
-				joinedAt = 0;
-				isJoining = false;
+				loadedAt = 0;
 				cancel();
 			}}, 0l, 100l);
 		});
