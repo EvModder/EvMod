47a7ddd091d35bb8d1c352b1b2d0500073aaf039
EvModder <evmodder@gmail.com>
2025-05-14 05:11:46 -0500

    wip caching changes for synched ignore list

---
+++ src/main/java/net/evmodder/KeyBound/IgnoreListSync2b2t.java
@@ -0,0 +1,73 @@
+package net.evmodder.KeyBound;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.UUID;
+import net.evmodder.EvLib.Command;
+import net.evmodder.EvLib.FileIO;
+import net.evmodder.EvLib.PacketHelper;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
+
+class IgnoreListSync2b2t{
+	private final boolean syncMyIgnored;
+	private final ArrayList<UUID> ignoreList;
+
+	private void handleIgnoreEvent(final String name, final boolean ignored){
+		Main.LOGGER.info("Ignore update: "+name+"="+ignored);
+		MinecraftClient client = MinecraftClient.getInstance();
+		PlayerListEntry ple = client.getNetworkHandler().getPlayerListEntry(name);
+		if(ple == null){Main.LOGGER.error("Unable to find PlayerListEntry for player name: "+name); return;}
+		final UUID ignoredUUID = ple.getProfile().getId();
+		if(ignored) ignoreList.add(ignoredUUID);
+		else ignoreList.remove(ignoredUUID);
+		if(syncMyIgnored){
+			final byte[] msg = ByteBuffer.allocate(17)
+					.putLong(ignoredUUID.getMostSignificantBits()).putLong(ignoredUUID.getLeastSignificantBits())
+					.put((byte)(ignored ? 0xFF : 0)).array();
+			Main.remoteSender.sendBotMessage(Command.DB_PLAYER_STORE_IGNORE, /*udp=*/true, 2000, msg, /*recv=*/null);
+		}
+	}
+
+	IgnoreListSync2b2t(final boolean shareMyIgnoreList, final String[] borrowIgnoreLists){
+		syncMyIgnored = shareMyIgnoreList;
+		ignoreList = new ArrayList<>();
+		final byte[] data = FileIO.loadFileBytes("2b2t_ignorelist");
+		if(data != null){
+			final int numIdsInFile = data.length / 16;
+			ignoreList.ensureCapacity(numIdsInFile);
+			if(numIdsInFile*16 != data.length) Main.LOGGER.error("Corrupted/unrecognized map group file");
+			ignoreList.ensureCapacity(numIdsInFile);
+			final ByteBuffer bb = ByteBuffer.wrap(data);
+			for(int i=0; i<numIdsInFile; ++i) ignoreList.add(new UUID(bb.getLong(), bb.getLong()));
+		}
+		if(borrowIgnoreLists != null) for(String name : borrowIgnoreLists){
+			UUID uuid;
+			try{uuid = UUID.fromString(name);}
+			catch(IllegalArgumentException e){
+				Main.LOGGER.error("Usernames for 'include_2b2t_ignore_lists' are not yet supported, please provide UUIDs instead");
+				continue;
+			}
+			Main.remoteSender.sendBotMessage(Command.DB_PLAYER_FETCH_IGNORES, /*udp=*/false, 20_000, PacketHelper.toByteArray(uuid), reply -> {
+				if(reply == null || reply.length % 16 != 0){
+					Main.LOGGER.warn("Fetch ignore list for '"+name+"' got invalid response! "+(reply==null?"null":"len%16 != 0"));
+					return;
+				}
+				final ByteBuffer bb = ByteBuffer.wrap(reply);
+				MinecraftClient client = MinecraftClient.getInstance();
+				for(int i=0; i<reply.length/16; ++i){
+					final UUID ignoredUUID = new UUID(bb.getLong(), bb.getLong());
+					if(!ignoreList.contains(ignoredUUID)) client.getNetworkHandler().sendChatCommand("/ignore "+ignoredUUID);
+				}
+			});
+		}
+		ClientReceiveMessageEvents.GAME.register((msg, overlay) -> {
+			Main.LOGGER.info("GAME Message: {}, overlay: {}", msg, overlay);
+			final String literal = msg.getLiteralString();
+			if(literal == null) return;
+			if(literal.startsWith("Now ignoring ")) handleIgnoreEvent(literal.substring(13), true);
+			if(literal.startsWith("No longer ignoring ")) handleIgnoreEvent(literal.substring(19, literal.length()-1), false);
+		});
+	}
+}
