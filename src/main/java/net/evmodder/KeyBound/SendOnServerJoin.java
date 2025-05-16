363dcc8dfb7fef546bb6e6f21b0ba17c7a1a41b7
EvModder <evmodder@gmail.com>
2025-05-15 23:16:08 -0500

    Wait for player to move before sending SendOnJoin commands

--- src/main/java/net/evmodder/KeyBound/SendOnServerJoin.java
+++ src/main/java/net/evmodder/KeyBound/SendOnServerJoin.java
@@ -5,11 +5,14 @@ import java.util.TimerTask;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.text.Text;
 
 public class SendOnServerJoin{
-	private final long JOIN_DELAY = 2000l;
+	private final long JOIN_DELAY = 2500l;
 	private long loadedAt/*, joinedAt*/;
+	private double loadedAtX, loadedAtZ;
 	private TimerTask timerTask;
+	private final boolean WAIT_FOR_MOVEMENT = true;
 
 	SendOnServerJoin(String[] messages){
 		ClientPlayConnectionEvents.JOIN.register(
@@ -27,13 +30,20 @@ public class SendOnServerJoin{
 				MinecraftClient client  = MinecraftClient.getInstance();
 				if(!client.isFinishedLoading() || client.player == null || client.world == null || client.getNetworkHandler() == null || !client.player.isAlive()
 					|| !client.player.isLoaded() || client.player.isSpectator() || client.player.isRegionUnloaded() || client.player.isInvisible()) return;
+				if(loadedAt == 0){
+					loadedAt = System.currentTimeMillis();
+					if(WAIT_FOR_MOVEMENT){loadedAtX = client.player.getX(); loadedAtZ = client.player.getZ();}
+					return;
+				}
 
-				//Main.LOGGER.info("Stuff seems loaded, checking JOIN_DELAY");
-				final long now = System.currentTimeMillis();
-				if(loadedAt == 0){loadedAt = now; return;}
-				else if(now - loadedAt < JOIN_DELAY) return;
+				//Main.LOGGER.info("Stuff seems loaded, waiting for player movement");
+				if(WAIT_FOR_MOVEMENT && client.player.getX() == loadedAtX && client.player.getZ() == loadedAtZ) return;
+
+				//Main.LOGGER.info("Player movement detected, checking JOIN_DELAY");
+				if(System.currentTimeMillis() - loadedAt < JOIN_DELAY) return;
 
 				//Main.LOGGER.info("JOIN_DELAY reached, triggering commands...");
+				client.player.sendMessage(Text.of("Sending "+messages.length+" msgs/cmds"), false);
 				ClientPlayNetworkHandler handler = client.getNetworkHandler();
 				for(String msg : messages){
 					msg = msg.trim();
