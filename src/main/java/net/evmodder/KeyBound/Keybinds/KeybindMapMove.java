3a7301dab2b0b0a450b123fa05ddced81d033c99
EvModder <evmodder@gmail.com>
2025-06-11 17:12:21 -0500

    Selective-move for map shulk (fancy new detection, we will see if it works lol)

--- src/main/java/net/evmodder/KeyBound/Keybinds/KeybindMapMove.java
+++ src/main/java/net/evmodder/KeyBound/Keybinds/KeybindMapMove.java
@@ -2,6 +2,8 @@ package net.evmodder.KeyBound.Keybinds;
 
 import java.util.ArrayDeque;
 import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.stream.IntStream;
 import net.evmodder.KeyBound.Main;
 import net.evmodder.KeyBound.Keybinds.ClickUtils.ClickEvent;
 import net.minecraft.client.MinecraftClient;
@@ -9,26 +11,28 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.ShulkerBoxScreen;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.MapIdComponent;
+import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.screen.slot.Slot;
+import net.minecraft.item.Items;
+import net.minecraft.item.map.MapState;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.Text;
-import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
 
 //TODO: Maybe preserve relative position of maps (eg., in a 3x3, keep them in a 3x3 in result GUI)?
 
 public final class KeybindMapMove{
-	private final boolean isMapArt(ItemStack stack){
-		if(stack == null || stack.isEmpty()) return false;
-		return Registries.ITEM.getId(stack.getItem()).getPath().equals("filled_map");
-	}
 
-//	private MapState getMapState(World world, ItemStack stack){
-//		if(stack == null || stack.isEmpty()) return null;
-//		if(!Registries.ITEM.getId(stack.getItem()).getPath().equals("filled_map")) return null;
-//		return FilledMapItem.getMapState(stack, world);
-//	}
+	private boolean isSpaceFillerMap(World world, ItemStack stack){
+		if(stack.getCustomName() == null) return false;
+		String name = stack.getCustomName().getLiteralString();
+		if(name == null || !name.startsWith("slot")) return false;
+		MapState state = FilledMapItem.getMapState(stack, world);
+		if(state == null) return false;//Rather a false negative than a false positive
+		byte[] colors = state.colors;
+		for(int i=1; i<colors.length; ++i) if(colors[i] != colors[i-1]) return false;
+		return true;
+	}
 
 	private boolean ongoingStealStore;
 	private long lastStealStore = 0;
@@ -40,75 +44,101 @@ public final class KeybindMapMove{
 		if(!(client.currentScreen instanceof ShulkerBoxScreen sbs)){/*Main.LOGGER.warn("MapMove cancelled: Not in ShulkerBoxScreen"); */return;}
 		//
 		final long ts = System.currentTimeMillis();
-		if(ts - lastStealStore < stealStoreCooldown){/*Main.LOGGER.warn("MapMove cancelled: Cooldown, "+ts+","+lastStealStore+","+stealStoreCooldown); */return;}
+		if(ts - lastStealStore < stealStoreCooldown){
+			//Main.LOGGER.warn("MapMove cancelled: Cooldown, "+ts+","+lastStealStore+","+stealStoreCooldown); 
+			return;
+		}
 		lastStealStore = ts;
 		//
-		final DefaultedList<Slot> slots = sbs.getScreenHandler().slots;
-		int numInInv = 0, emptySlotsInv = 0, sameCountInv = 0;
-		HashMap<Integer, Integer> mapsInPlayerInv = new HashMap<>();// id -> available stackable space
+		final ItemStack[] slots = sbs.getScreenHandler().slots.stream().map(s -> s.getStack()).toArray(ItemStack[]::new);
+		int numInInv = 0, emptySlotsInv = 0;
+		TreeSet<Integer> countsInInv = new TreeSet<>();
+		HashMap<Integer, Integer> mapIdsInInv = new HashMap<>();// id -> available stackable space
 		for(int i=0; i<36; ++i){
 			ItemStack stack = client.player.getInventory().getStack(i);
 			if(stack == null || stack.isEmpty()) ++emptySlotsInv;
-			else if(isMapArt(stack)){
+			else if(stack.getItem() == Items.FILLED_MAP){
+				if(isSpaceFillerMap(client.world, stack)) continue;
+				++numInInv;
+				final int count = stack.getCount();
+				countsInInv.add(count);
 				MapIdComponent id = stack.get(DataComponentTypes.MAP_ID);
 				if(id == null){Main.LOGGER.error("MapMove cancelled: Unloaded map in player inventory (!!!)"); return;}
-				int space = mapsInPlayerInv.getOrDefault(id.id(), 0);
-				mapsInPlayerInv.put(id.id(), space+(stack.getMaxCount()-stack.getCount()));
-				++numInInv;
-				if(sameCountInv == 0) sameCountInv = stack.getCount();
-				else if(sameCountInv != stack.getCount()) sameCountInv = -1; // Different map slots with different counts => -1
+				mapIdsInInv.put(id.id(), mapIdsInInv.getOrDefault(id.id(), 0)+(stack.getMaxCount()-count));
 			}
 		}
-		boolean cantMergeMapsIntoShulker = false;
-		int numInShulk = 0, emptySlotsShulk = 0, sameCountShulk = 0;
+		int cantMergeIntoInv = 0;
+		int numInShulk = 0, emptySlotsShulk = 0;
+		TreeSet<Integer> countsInShulk = new TreeSet<>();
+		HashMap<Integer, Integer> mapIdsInShulker = new HashMap<>();
 		for(int i=0; i<27; ++i){
-			ItemStack stack = slots.get(i).getStack();
-			if(stack == null || stack.isEmpty()) ++emptySlotsShulk;
-			else if(isMapArt(stack)){
+			ItemStack stack = slots[i];
+			if(stack.isEmpty()) ++emptySlotsShulk;
+			else if(stack.getItem() == Items.FILLED_MAP){
+				if(isSpaceFillerMap(client.world, slots[i])) continue;
 				++numInShulk;
-				if(sameCountShulk == 0) sameCountShulk = stack.getCount();
-				else if(sameCountShulk != stack.getCount()) sameCountShulk = -1; // Different map slots with different counts => -1
-
-				if(cantMergeMapsIntoShulker) continue;
+				final int count = slots[i].getCount();
+				countsInShulk.add(count);
 				MapIdComponent id = stack.get(DataComponentTypes.MAP_ID);
-				if(id == null){cantMergeMapsIntoShulker = true; continue;}
-				Integer space = mapsInPlayerInv.get(id.id());
-				if(space == null || space - stack.getCount() < 0) cantMergeMapsIntoShulker = true;
-				else mapsInPlayerInv.put(space, space - stack.getCount());
+				//if(id == null){Main.LOGGER.warn("MapMove cancelled: Unloaded map in shulker"); return;}
+				if(id == null){++cantMergeIntoInv; continue;}
+				mapIdsInShulker.put(id.id(), mapIdsInInv.getOrDefault(id.id(), 0)+(stack.getMaxCount()-count));
+				Integer space = mapIdsInInv.get(id.id());
+				if(space == null || count > space) ++cantMergeIntoInv;
+				else mapIdsInInv.put(space, space - count);
 			}
 		}
-		boolean moveToShulk = true;
+
+		long cantMergeIntoShulk =
+				IntStream.range(0, 36).mapToObj(i -> client.player.getInventory().getStack(i))
+				.filter(s -> s.getItem() == Items.FILLED_MAP)
+				.filter(s -> !isSpaceFillerMap(client.world, s))
+				.filter(s -> {
+					MapIdComponent id = s.get(DataComponentTypes.MAP_ID);
+					Integer space = mapIdsInShulker.get(id.id());
+					if(space == null || s.getCount() > space) return false;
+					mapIdsInShulker.put(space, space - s.getCount());
+					return true;
+				}).count();
+
 		if(numInShulk == 0 && numInInv == 0){Main.LOGGER.warn("MapMove cancelled: No mapart found"); return;}
-		if(numInShulk != 0 && numInInv != 0 && (numInInv != numInShulk || cantMergeMapsIntoShulker
-				|| sameCountShulk == -1 || sameCountInv == -1 || sameCountShulk+sameCountInv > 64)){
+		if(cantMergeIntoShulk != 0 && cantMergeIntoInv != 0){
 			Main.LOGGER.warn("MapMove cancelled: Unique mapart found in both inventory AND shulker");
 			return;
 		}
-		if(numInInv == 0){
-			moveToShulk = false;
-			if(numInShulk > emptySlotsInv){Main.LOGGER.warn("MapMove cancelled: Not enough empty slots in inventory"); return;}
-		}
-		else if(numInInv > emptySlotsShulk && cantMergeMapsIntoShulker){Main.LOGGER.warn("MapMove cancelled: Not enough empty slots in shulker"); return;}
+		if(numInInv == 0 && cantMergeIntoInv > emptySlotsInv){Main.LOGGER.warn("MapMove cancelled: Not enough empty slots in inventory"); return;}
+		if(numInShulk == 0 && cantMergeIntoShulk > emptySlotsShulk){Main.LOGGER.warn("MapMove cancelled: Not enough empty slots in shulker"); return;}
+
+		boolean moveToShulk = numInShulk == 0 || cantMergeIntoInv > emptySlotsInv;
 		//
 		final boolean isShiftClick = Screen.hasShiftDown();
 		ArrayDeque<ClickEvent> clicks = new ArrayDeque<>();
 		HashMap<ClickEvent, Integer> reserveClicks = new HashMap<>();
+		final boolean selectiveMove = !isShiftClick && moveToShulk
+				? (countsInInv.size() == 2 && cantMergeIntoShulk == 0)
+				: (countsInShulk.size() == 2 && (cantMergeIntoInv == 0 || numInInv == 0));
 		if(moveToShulk) for(int i=27; i<63; ++i){
-			if(!isMapArt(slots.get(i).getStack())) continue;
-			if((sameCountInv == 2 || sameCountInv == 3) && !isShiftClick){
+			if(slots[i].getItem() != Items.FILLED_MAP) continue;
+			if(isSpaceFillerMap(client.world, slots[i])) continue;
+			final int count = slots[i].getCount();
+			if(selectiveMove && count != countsInInv.last()) continue;
+			if((count == 2 || count == 3) && !isShiftClick){
 				clicks.add(new ClickEvent(i, 1, SlotActionType.PICKUP)); // right-click: pickup half
 				if(Main.inventoryUtils.MAX_CLICKS >= 3) reserveClicks.put(clicks.peekLast(), 3);
 			}
 			clicks.add(new ClickEvent(i, 0, SlotActionType.QUICK_MOVE)); // shift-click: all in slot to shulker
-			if((sameCountInv == 2 || sameCountInv == 3) && !isShiftClick){
+			if((count == 2 || count == 3) && !isShiftClick){
 				clicks.add(new ClickEvent(i, 0, SlotActionType.PICKUP)); // left-click: put back all on cursor
 			}
 		}
 		else for(int i=26; i>=0; --i){
 //			if(isMapArt(sh.getSlot(i).getStack())) clicks.add(new ClickEvent(sh.syncId, i, 0, SlotActionType.QUICK_MOVE));
-			if(!isMapArt(slots.get(i).getStack())) continue;
-			if(sameCountShulk > 1 && !isShiftClick){
-				if(sameCountShulk <= 3){
+			if(slots[i].getItem() != Items.FILLED_MAP) continue;
+			if(isSpaceFillerMap(client.world, slots[i])) continue;
+			final int count = slots[i].getCount();
+			if(selectiveMove && count != countsInInv.last()) continue;
+			if(count > 1 && !isShiftClick){
+				if(count <= 3){
 					clicks.add(new ClickEvent(i, 1, SlotActionType.PICKUP)); // right-click: pickup half
 					if(Main.inventoryUtils.MAX_CLICKS >= 3) reserveClicks.put(clicks.peekLast(), 3);
 				}
@@ -119,7 +149,7 @@ public final class KeybindMapMove{
 				}
 			}
 			clicks.add(new ClickEvent(i, 0, SlotActionType.QUICK_MOVE));
-			if(sameCountShulk > 1 && !isShiftClick){
+			if(count > 1 && !isShiftClick){
 				clicks.add(new ClickEvent(i, 0, SlotActionType.PICKUP)); // left-click: place all
 			}
 		}
