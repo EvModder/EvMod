cf32ed91f8e2c43f646951f2f8ecfd4983b576e5
EvModder <evmodder@gmail.com>
2025-10-04 15:45:18 -0500

    Add `getSync()` to LoadingCache

--- src/main/java/net/evmodder/EvLib/LoadingCache.java
+++ src/main/java/net/evmodder/EvLib/LoadingCache.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 
 public abstract class LoadingCache<K, V>{
 	private final HashMap<K, V> cache;
+	//TODO: loading duration tracking?
 	private final HashMap<K, Thread> loading = new HashMap<>();
+	@SuppressWarnings("unused")
 	private final V V_NOT_FOUND, V_LOADING;
 	public LoadingCache(final HashMap<K, V> initMap, final V notFound, final V loading){cache = initMap; V_NOT_FOUND = notFound; V_LOADING = loading;}
 	public LoadingCache(final V notFound, final V loading){this(new HashMap<>(), notFound, loading);}
@@ -25,30 +27,63 @@ public abstract class LoadingCache<K, V>{
 		if(t != null) t.interrupt();
 		return true;
 	}
-	public final boolean containsKey(final K k){return cache.containsKey(k);}
+//	public final boolean containsKey(final K k){return cache.containsKey(k);}
 	public final V get(final K k){
 		if(cache.containsKey(k)){
-			final V v = cache.get(k);
-			return v != null ? v : V_NOT_FOUND;
+			return cache.get(k);
+//			final V v = cache.get(k);
+//			if(v == null) assert V_NOT_FOUND == null;
+//			return v != null ? v : V_NOT_FOUND;
 		}
 		{
 			final V v = loadSyncOrNull(k);
-			if(v != null){cache.put(k, v); return v;}
+			if(v != null){
+				assert v != V_LOADING : "loadSyncOrNull() failed to do what its name implies (didn't load, yet didn't return null";
+				cache.put(k, v);
+				return v;
+			}
 		}
-		if(!loading.containsKey(k)){
-			final Thread t = new Thread(()->{
-				final V v = load(k);
-				if(v.equals(V_LOADING)){
-					synchronized(loading){if(loading.containsKey(k)) loading.put(k, null);} // null out this Thread so it can be garbage collected
-					return; // Assume the caller will call putIfAbsent() once a value is loaded
-				}
-				synchronized(cache){cache.put(k, v);}
-				synchronized(loading){loading.remove(k);}
-			});
-			loading.put(k, t);
-			t.start();
+		synchronized(loading){
+			if(!loading.containsKey(k)){
+				final Thread t = new Thread(()->{
+					final V v = load(k);
+					if(v.equals(V_LOADING)){
+						synchronized(loading){if(loading.containsKey(k)) loading.put(k, null);} // null out this Thread so it can be garbage collected
+						return; // Assume the caller will call putIfAbsent() once a value is loaded
+					}
+					synchronized(cache){cache.put(k, v);}
+					synchronized(loading){loading.remove(k);}
+				});
+				loading.put(k, t);
+				t.start();
+			}
 		}
 		return V_LOADING;
 	}
-	//TODO: private final HashMap<K, Pair</*timestamp*/long, V>> cache
+	public final V getSync(final K k){
+		if(cache.containsKey(k)) return cache.get(k);
+		{
+			final V v = loadSyncOrNull(k);
+			if(v != null){
+				assert v != V_LOADING : "loadSyncOrNull() failed to do what its name implies (didn't load, yet didn't return null";
+				cache.put(k, v);
+				return v;
+			}
+		}
+		final boolean iAmTheLoader;
+//		assert Thread.currentThread() != null; // iirc, main thread id=1
+		synchronized(loading){iAmTheLoader = loading.putIfAbsent(k, Thread.currentThread()) == null;}
+		if(iAmTheLoader){
+			final V v = load(k);
+			if(!v.equals(V_LOADING)){
+				synchronized(cache){cache.put(k, v);}
+				synchronized(loading){loading.remove(k);}
+				return v;
+			}
+		}
+		else while(loading.containsKey(k)) Thread.yield();
+		// Sometimes even load() itself is async/callback driven and not guaranteed to give a value...
+		while(!cache.containsKey(k)) Thread.yield(); // So we just wait for someone to call putIfAbsent()
+		return cache.get(k);
+	}
 }
