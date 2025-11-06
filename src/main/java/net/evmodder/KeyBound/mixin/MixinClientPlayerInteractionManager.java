8c741fbc4e5b9f7f7b41d8b865677c586bf5ba5b
EvModder <evmodder@gmail.com>
2025-11-06 13:41:56 -0600

    Some BS nonsense with fake extra clicks getting sent? idk just pretend the dont exist and hope nothing breaks

--- src/main/java/net/evmodder/KeyBound/mixin/MixinClientPlayerInteractionManager.java
+++ src/main/java/net/evmodder/KeyBound/mixin/MixinClientPlayerInteractionManager.java
@@ -52,10 +52,13 @@ abstract class MixinClientPlayerInteractionManager{
 	@Inject(method = "clickSlot", at = @At("HEAD"), cancellable = true)
 	private void avoid_sending_too_many_clicks(int syncId, int slot, int button, SlotActionType action, PlayerEntity player, CallbackInfo ci){
 //		if(action == SlotActionType.THROW/* || action == SlotActionType.CLONE || action == SlotActionType.QUICK_CRAFT*/) return;
+		if(slot == -999) return; // TODO: comment this out to test things
 		final boolean isBotted = Main.clickUtils.isThisClickBotted(/*friend*/);
 		if(Configs.Generic.CLICK_FILTER_USER_INPUT.getBooleanValue() && !isBotted && Main.clickUtils.hasOngoingClicks()){
 			ci.cancel();
+			if(syncId == 0 && slot == 0 && button == 0 && action == SlotActionType.QUICK_MOVE) return; // QUICK_CRAFT sometimes sends duplicate fake QUICK_MOVE?
 			MinecraftClient.getInstance().player.sendMessage(Text.literal("Discarding user click to protect an ongoing ClickOp").withColor(/*&c=*/16733525), false);
+			MinecraftClient.getInstance().player.sendMessage(Text.literal("syncId="+syncId+",slot="+slot+",button="+button+",action="+action.name()), false);
 			return;
 		}
 		final int availableClicks = Main.clickUtils.calcAvailableClicks();
@@ -66,11 +69,13 @@ abstract class MixinClientPlayerInteractionManager{
 			if(isBotted) Main.LOGGER.error("Botted click somehow triggered click limited! VERY BAD!!");
 			else if(!Configs.Generic.CLICK_LIMIT_USER_INPUT.getBooleanValue()) return;
 			ci.cancel(); // Throw out clicks that exceed the limit!!
+			if(syncId == 0 && slot == 0 && button == 0 && action == SlotActionType.QUICK_MOVE) return; // QUICK_CRAFT sometimes sends duplicate fake QUICK_MOVE?
 			Main.LOGGER.error("Discarded click in clickSlot() due to exceeding limit!"
 					+ " slot:"+slot+",button:"+button+",action:"+action.name()+",isShiftClick:"+Screen.hasShiftDown());
 			MinecraftClient.getInstance().player.sendMessage(
 					Text.literal("Discarding unsafe clicks!! > LIMIT:"+Configs.Generic.CLICK_LIMIT_COUNT.getIntegerValue()
 								+", available:"+availableClicks).withColor(/*&c=*/16733525), false);
+			MinecraftClient.getInstance().player.sendMessage(Text.literal("syncId="+syncId+",slot="+slot+",button="+button+"action="+action.name()), false);
 		}
 	}
 }
