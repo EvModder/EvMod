573c796d299a59f1970376a88ac1fff39a245093
EvModder <evmodder@gmail.com>
2025-12-24 23:20:44 -0600

    Make it harder to accidentally leak coords when streaming with new map notifier

--- src/main/java/net/evmodder/evmod/apis/NewMapNotifier.java
+++ src/main/java/net/evmodder/evmod/apis/NewMapNotifier.java
@@ -6,6 +6,7 @@ import net.evmodder.evmod.Main;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
 
 public final class NewMapNotifier{
@@ -20,7 +21,9 @@ public final class NewMapNotifier{
 		if(System.currentTimeMillis() - lastNewMapNotify < mapNotifyCooldown) return;
 
 		// TODO: play sound?
-		String pos = ife.getBlockX()+" "+ife.getBlockY()+" "+ife.getBlockZ();
+		boolean isFar = ife.getBlockPos().getSquaredDistance(0, 0, 0) > 20_000d*20_000d;
+		int x = ife.getBlockX(), z = ife.getBlockZ();
+		String pos = (isFar ? ".."+Math.abs(x%1000) : x)+" "+ife.getBlockY()+" "+(isFar ? ".."+Math.abs(z%1000) : z);
 
 		int color = Configs.Visuals.MAP_COLOR_NOT_IN_GROUP.getIntegerValue();
 		MinecraftClient.getInstance().player.sendMessage(Text.literal("New mapart: "+pos).withColor(color), true);
@@ -31,10 +34,12 @@ public final class NewMapNotifier{
 			if(notifyInChat){
 				MinecraftClient.getInstance().player.sendMessage(
 						Text.literal("New mapart: "+pos)
-						.append(Text.literal(" \u2398 ") // unicode for symbol ⎘
-							.styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, pos))))
-						.append(ife.getHeldItemStack().getName()).withColor(color)
-						, false);
+						// unicode for symbol ⎘
+						.append(Text.literal(" \u2398 ").styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, pos))))
+						// Hover shows map itemname
+						.withColor(color)
+						.styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, ife.getHeldItemStack().getName())))
+						, /*actionbar=*/false);
 			}
 		}
 		lastNewMapNotify = System.currentTimeMillis();
